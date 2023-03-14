07c9166f03f304f26e44729e3d88871b58b01388
alikpid <koromislo.molodec@gmail.com>
2023-03-14 13:57:58 +0700

    чекбоксы испралены, все переходы работают, ошибки норм выводядся. размести норм форму добавления и почисти код

--- js/main.js
+++ js/main.js
@@ -16,13 +16,13 @@ Vue.component('col3', {
    <div class="column3">
    <h3>In progress</h3>
         <div class="error" v-for="error in errors">{{error}}</div>
-       <ul class="note" v-for="note in column3">
+        <ul class="note" v-for="note in column3">
             <li>{{note.title}}  
             <ol>
                 <li class="items" v-for="item in note.noteItems" v-if="item.title != null">
                     {{item.title}}
                 </li>
-                    <p>{{note.date}}</p>  <!-- :class="{completed: item.completed}" -->
+                    <span class="dateNote">{{note.date}}</span>  <!-- :class="{completed: item.completed}" -->
             </ol>
             </li>
         </ul>
@@ -37,26 +37,24 @@ Vue.component('col2', {
         column2: {
             type: Array,
         },
-        column3: {
-            type: Array,
-        },
         note: {
             type: Object
         },
-        errors: {
+        errors2: {
             type: Array
         }
     },
+
     template: `
    <div class="column2">
    <h3>In progress</h3>
-        <div class="error" v-for="error in errors">{{error}}</div>
-       <ul class="note" v-for="note in column2">
+        <div class="error" v-for="error in errors2" :key="error.name">{{error}}</div>
+       <ul class="note" v-for="note in column2" :key="note.date">
             <li>{{note.title}}  
             <ol>
-                <li class="items" v-for="item in note.noteItems" v-if="item.title != null">
-                    <span :class="{completed: item.completed}">{{item.title}}</span>
-                    <input type="checkbox" @click="changeAchievement(note, item)">  <!-- :class="{completed: item.completed}" -->
+                <li class="items" v-for="item in note.noteItems" :key="item.title">
+                    <label for="item">{{item.title}}</label>
+                    <input type="checkbox" :checked="item.completed" @click="changeAchievement(note, item)" id="item">  <!-- :class="{completed: item.completed}" -->
                 </li>
             </ol>
             </li>
@@ -68,21 +66,12 @@ Vue.component('col2', {
     methods: {
         changeAchievement(note, item) {
             item.completed = true
-            note.status = 0
-            let countOfItems = 0
-            for(let i = 0; i < 3; i++){
-                if (note.noteItems[i].title !== null) {
-                    countOfItems++
-                }
-            }
-            for (let i = 0; i < countOfItems; ++i) {
-                if (note.noteItems[i].completed === true) {
-                    note.status++;
-                }
-            }
-            console.log(note.status)
-            if ((note.status / countOfItems) * 100 === 100) {
-                eventBus.$emit('addToCol3', note),
+            note.progress = 0
+            for (let i = 0; i < note.noteItems.length; ++i)
+                if (note.noteItems[i]?.completed === true)
+                    note.progress++;
+            if ((note.progress / note.noteItems.length) * 100 === 100) {
+                eventBus.$emit('addToCol3', note);
                 note.date = new Date().toLocaleString();
             }
 
@@ -95,9 +84,6 @@ Vue.component('col1', {
         column1: {
             type: Array,
         },
-        column2: {
-            type: Array,
-        },
         note: {
             type: Object
         },
@@ -108,53 +94,42 @@ Vue.component('col1', {
     template: `
    <div class="column1">
    <h3>To do</h3>
-        <div class="error" v-for="error in errors">{{error}}</div>
-       <ul class="note" v-for="note in column1">
+        <div class="error" v-for="error in errors" :key="error.name">{{error}}</div>
+       <ol class="note" v-for="note in column1" :key="note.date">
             <li>{{note.title}}  
             <ol>
-                <li class="items" v-for="item in note.noteItems" v-if="item.title != null">
-                    <span :class="{completed: item.completed}">{{item.title}}</span>
-                    <input type="checkbox" @click="changeAchievement(note, item)">  <!-- :class="{completed: item.completed}" -->
+                <li class="items" v-for="item in note.noteItems" :key="item.title">
+                    <label for="item">{{item.title}}</label>
+                    <input type="checkbox" :checked="item.completed" @click="changeAchievement(note, item)" id="item">  <!-- :class="{completed: item.completed}" -->
                 </li>
             </ol>
             </li>
-        </ul>
+        </ol>
    </div>
     `,
     methods: {
         changeAchievement(note, item) {
             item.completed = true
-            note.status = 0
-            let countOfItems = 0
-            for(let i = 0; i < 3; i++){
-                if (note.noteItems[i].title !== null) {
-                    countOfItems++
-                    // console.log(countOfItems)
-                }
-            }
-            for (let i = 0; i < countOfItems; ++i) {
-                if (note.noteItems[i].completed === true) {
-                    note.status++;
-                }
-            }
-            console.log(note.status)
-            if ((note.status / countOfItems) * 100 >= 50) {
-                eventBus.$emit('addToCol2', note)
-            }
+            note.progress = 0
 
+            for (let i = 0; i < note.noteItems.length; ++i)
+                if (note.noteItems[i].completed === true)
+                    note.progress++;
+            if ((note.progress / note.noteItems.length) * 100 >= 50)
+                eventBus.$emit('addToCol2', note)
         },
     },
 })
 
 Vue.component('note-board', {
-    template:`
+    template: `
     <div class="noteBoard">
     
         <col1 :column1="column1" :errors="errors"></col1>
 
-        <col2 :column2="column2" :errors="errors"></col2>
+        <col2 :column2="column2" :errors2="errors2"></col2>
         
-        <col3 :column3="column3" :errors="errors"></col3>
+        <col3 :column3="column3"></col3>
 
        
     </div>
@@ -163,19 +138,21 @@ Vue.component('note-board', {
         return {
             column1: [],
             column2: [],
-            column3: [],//не забудь потом еще колонки
-            errors: []
+            column3: [],
+            errors: [],
+            errors2: []
         }
     },
     mounted() {
         this.column1 = JSON.parse(localStorage.getItem('column1')) || [];
+        this.column2 = JSON.parse(localStorage.getItem('column2')) || [];
+        this.column3 = JSON.parse(localStorage.getItem('column3')) || [];
         eventBus.$on('addToCol1', note => {
             this.errors = []
             if (this.column1.length < 3) {
                 this.column1.push(note);
                 this.saveColumn1();
-            }
-            else
+            } else
                 this.errors.push('No more than 3 notes in the first column')
         })
         eventBus.$on('addToCol2', note => {
@@ -183,18 +160,29 @@ Vue.component('note-board', {
             if (this.column2.length < 5) {
                 this.column2.push(note)
                 this.column1.splice(this.column1.indexOf(note), 1)
+                this.saveColumn2();
+                this.saveColumn1();
             } else {
-                this.errors.push("No more than 5 notes in the second column")
+                this.errors2.push("No more than 5 notes in the second column")
+
             }
         })
         eventBus.$on('addToCol3', note => {
             this.column3.push(note)
             this.column2.splice(this.column2.indexOf(note), 1)
+            this.saveColumn3();
+            this.saveColumn2();
         })
     },
     methods: {
-        saveColumn1(){
+        saveColumn1() {
             localStorage.setItem('column1', JSON.stringify(this.column1));
+        },
+        saveColumn2() {
+            localStorage.setItem('column2', JSON.stringify(this.column2));
+        },
+        saveColumn3() {
+            localStorage.setItem('column3', JSON.stringify(this.column3));
         }
     }
 })
@@ -215,19 +203,27 @@ Vue.component('new-note', {
             
             <p>
                 <label for="title">Title</label>
-                <input type="text" id="title" v-model="title">
+                <input type="text" id="title" v-model.trim="title">
             </p>
             <p>
                 <label for="item1">Item №1</label>
-                <input type="text" id="item1" v-model="noteItem1">
+                <input type="text" id="item1" v-model.trim="noteItem1">
             </p>
             <p>
                 <label for="item2">Item №2</label>
-                <input type="text" id="item2" v-model="noteItem2">
+                <input type="text" id="item2" v-model.trim="noteItem2">
             </p>
             <p>
                 <label for="item3">Item №3</label>
-                <input type="text" id="item3" v-model="noteItem3">
+                <input type="text" id="item3" v-model.trim="noteItem3">
+            </p>
+            <p>
+                <label for="item4">Item №4</label>
+                <input type="text" id="item4" v-model.trim="noteItem4">
+            </p>
+            <p>
+                <label for="item5">Item №5</label>
+                <input type="text" id="item5" v-model.trim="noteItem5">
             </p>
 
             <button type="submit">Add</button>
@@ -241,31 +237,38 @@ Vue.component('new-note', {
             noteItem1: null,
             noteItem2: null,
             noteItem3: null,
+            noteItem4: null,
+            noteItem5: null,
             errorsForm: [],
             errors: []
         }
     },
     methods: {
         onSubmit() {
-            if(this.title && this.noteItem1 && this.noteItem2) {
+            const noteItems = [];
+            this.noteItem1 ? noteItems.push({title: this.noteItem1, completed: false}) : "";
+            this.noteItem2 ? noteItems.push({title: this.noteItem2, completed: false}) : "";
+            this.noteItem3 ? noteItems.push({title: this.noteItem3, completed: false}) : "";
+            this.noteItem4 ? noteItems.push({title: this.noteItem4, completed: false}) : "";
+            this.noteItem5 ? noteItems.push({title: this.noteItem5, completed: false}) : "";
+            if (this.title && this.noteItem1 && this.noteItem2 && this.noteItem3) {
                 let note = {
                     title: this.title,
-                    noteItems: [{title: this.noteItem1, completed: false},
-                                {title: this.noteItem2, completed: false},
-                                {title: this.noteItem3, completed: false}],
-                    date: null,
+                    noteItems,
+                    date: Date.now(),
                     errors: [],
-                    status: 0,
+                    progress: 0,
                 }
-                eventBus.$emit('addToCol1', note)  //добавление в первую колонку
+                eventBus.$emit('addToCol1', note)
                 this.title = null
                 this.noteItem1 = null
                 this.noteItem2 = null
                 this.noteItem3 = null
+                this.noteItem4 = null
+                this.noteItem5 = null
             } else {
-                if(!this.title) this.errorsForm.push("Title required.")
-                if(!this.noteItem1) this.errorsForm.push("Sub-item required.")
-                if(!this.noteItem2) this.errorsForm.push("Need at least two sub-items")
+                if (!this.title) this.errorsForm.push("Title required.")
+                if (!this.noteItem1 || !this.noteItem2 || !this.noteItem3) this.errorsForm.push("Need at least three sub-items")
             }
         }
     },
